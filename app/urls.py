415cf1003a7cfd36dc517916920a52c0fca087c2
FER <egorlymar1@gmail.com>
2024-05-23 23:51:42 +0300

    Added media to the project

--- app/urls.py
+++ app/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
-from app.settings import DEBUG
+# from django.conf import settings
+from app import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,7 +27,9 @@ urlpatterns = [
     path('catalog/', include('goods.urls', namespace='catalog')),
 ]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += [
         path('__debug__/', include('debug_toolbar.urls')),
     ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
