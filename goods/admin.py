2f47559340cae1aa0b57f621b40526fc344e44f3
FER <egorlymar1@gmail.com>
2024-05-23 10:51:14 +0300

    Create and register models: Categories, Products. Make migrations.

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categories, Products
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    prepopulated_fields = {'slug': ('name', )}
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'image', 'price', 'quantity', 'category')
+    list_display_links = ('id', 'name')
+    prepopulated_fields = {'slug': ('name',)}
--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    class Meta:
+        db_table = 'category'
+        ordering = ('id', )
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=150, unique=True, help_text='Название категории')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL', help_text='URL категории')
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    class Meta:
+        db_table = 'product'
+        ordering = ('category', )
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    name = models.CharField(max_length=150, unique=True, help_text='Название категории')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL', help_text='URL категории')
+    description = models.TextField(blank=True, null=True, help_text='Описание')
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, help_text='Изображение')
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, help_text='Цена')
+    discount = models.DecimalField(default=0.00, max_digits=4, decimal_places=2, help_text='Скидка')
+    quantity = models.PositiveIntegerField(default=0, help_text='Количество')
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, help_text='Категория')
+
+    def __str__(self):
+        return self.name
